5d0df8bc47c4f91f3176eb1b9006f7bc5a84c124
Denise <denusz@live.com.ar>
2022-04-14 23:56:55 -0300

    Agregando JS

---
+++ script.js
@@ -0,0 +1,5 @@
+if (numeroConsulta == "si"){
+    alert("Genial!! podes acceder a tu primer consulta GRATIS!");
+}  else {
+    alert("Gracias por elegirnos nuevamente!! Nos alegra muchisimo :)");
+}
